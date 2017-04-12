73d1ad4d7747134342bd5228dba0b66dfcb8994f
Aleksi Tommila <H8951@LABRANET.JAMK.FI>
2017-04-12 13:42:29 +0300

    Projekti alustettu, UI aloitettu

---
+++ Drunkster/src/drunkster/Drunkster.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drunkster;
+
+/**
+ *
+ * @author H8951
+ */
+public class Drunkster {
+
+    /**
+     * @param args the command line arguments
+     */
+   /* public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+    
+}
